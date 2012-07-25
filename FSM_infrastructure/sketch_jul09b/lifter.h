a5dcdd3f38c7b7cfe9a788fa54a8b8ef45ff3249
uberblok <blakeraymond.ubc@gmail.com>
2012-07-25 13:58:56 -0700

    Lifter

---
+++ FSM_infrastructure/sketch_jul09b/lifter.h
@@ -0,0 +1,108 @@
+#ifndef LIFTER_HEADER_GUARD
+#define LIFTER_HEADER_GUARD
+
+#include <signal.h>
+#include <pins.h>
+
+enum LifterPosition
+{
+	LOWERED,
+	STACKING,
+	RAISED
+};
+
+class Lifter
+{
+	public:
+		Lifter()
+		:bottomSwitch(LIFTER_SWITCH_BOT),
+		stackingSwitch(LIFTER_SWITCH_MID),
+		topSwitch(LIFTER_SWITCH_TOP)
+		{
+		
+		}
+		
+		void enable()
+		{
+			OBSERVER.addSignal((Signal*)&bottomSwitch);
+			OBSERVER.addSignal((Signal*)&stackingSwitch);
+			OBSERVER.addSignal((Signal*)&topSwitch);
+		}
+		
+		void disable()
+		{
+			OBSERVER.removeSignal((Signal*)&bottomSwitch);
+			OBSERVER.removeSignal((Signal*)&stackingSwitch);
+			OBSERVER.removeSignal((Signal*)&topSwitch);
+		}		
+		
+		void setTargetPosition(LifterPosition target)
+		{
+			targetPosition = target;
+		}
+		
+		void update()
+		{
+			updatePosition();
+			
+			if(targetPosition > currentPosition) //Higher
+			{
+				raiseStage();
+			}
+			else if(targetPosition < currentPosition)
+			{
+				lowerStage();
+			}
+			else // targetPosition == currentPosition
+			{
+				stopStage();
+			}
+		}
+		
+		bool ready()
+		{
+			return currentPosition == targetPosition;
+		}
+		
+		DigitalSignal bottomSwitch;
+		DigitalSignal stackingSwitch;
+		DigitalSignal topSwitch;
+		
+		LifterPosition currentPosition;
+		LifterPosition targetPosition;
+		
+	private:		
+		void raiseStage()
+		{
+			motor.speed(LIFTER_MOTOR, MAX_MOTOR_SPEED);
+		}
+		
+		void lowerStage()
+		{
+			motor.speed(LIFTER_MOTOR, -MAX_MOTOR_SPEED);
+		}
+		
+		void stopStage()
+		{
+			motor.stop(LIFTER_MOTOR);
+		}
+		
+		void updatePosition()
+		{
+			if(bottomSwitch.on())
+			{
+				currentPosition = LOWERED;
+			}
+			else if(stackingSwitch.on())
+			{
+				currentPosition = STACKING;
+			}
+			else if(raisedSwitch.on())
+			{
+				currentPosition = RAISED;
+			}
+		}
+	
+};
+
+#endif
